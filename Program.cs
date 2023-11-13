9edb249eac95d3065aa584cdd8eb037c0440ad92
Faith nkatha <faithnkatha160@gmail.com>
2023-11-14 01:15:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class QuestionFive
+{
+    static void Main()
+    {
+        char letter;
+       
+        Console.WriteLine("input a character");
+
+        letter=Convert.ToChar(Console.ReadLine());
+
+        if (char.IsLetter(letter))
+        {
+            if (char.IsUpper(letter))
+                Console.WriteLine($"{letter} in lower case is {char.ToLower(letter)}");
+            else 
+                Console.WriteLine($"{letter} in upper case is {char.ToUpper(letter)}");
+        }
+        else
+            Console.WriteLine("input a letter");
+    }
+}
